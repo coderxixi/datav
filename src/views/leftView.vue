9c4f7199889ef6bb6a32c1154591903c38f505aa
liuss7 <976344695@qq.com>
2023-05-05 13:48:07 +0800

    实现左侧组件菜单

--- src/views/leftView.vue
+++ src/views/leftView.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="wrap">
-    左边  <TextComp/>
+    <div class="meunItem " v-for="(item,index) of meunlist" :key="index">
+       {{item.name}}
+    </div>
+
   </div>
 </template>
 
 <script setup>
-
+import {ref} from "vue"
+import { menu } from "./menuList";
+const meunlist=ref(menu)
 </script>
 
 <style lang="less" scoped>
 .wrap{
   width: 250px;
+  .meunItem{
+    background-color: #eee;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    line-height: 100px;
+    cursor: pointer;
+    float: left;
+    margin: 10px;
+  }
 }
 </style>
---
+++ src/views/menuList.js
@@ -0,0 +1,22 @@
+export const menu= [
+  {
+    type:'TextComp',
+    name:'文本组件'
+  },
+  {
+    type: 'TextComp',
+    name: '文本组件'
+  },
+   {
+    type: 'TextComp',
+    name: '文本组件'
+  },
+  {
+    type: 'TextComp',
+    name: '文本组件'
+  },
+  {
+    type: 'TextComp',
+    name: '文本组件'
+  }
+]
